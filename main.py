496657a60e33d3c67955d0a368465d164c26118b
Hazem-Ahmed-Salem <hazem.salem.work@gmail.com>
2025-11-28 18:35:10 +0200

    finished the bookbot

--- main.py
+++ main.py
@@ -7,6 +7,3 @@ try:
 except:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
-# words_num = num_of_words("books/frankenstein.txt") 
-# char = num_of_letters("books/frankenstein.txt")
-# print(f"Found {words_num} total words")
